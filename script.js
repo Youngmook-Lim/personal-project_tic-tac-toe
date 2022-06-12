8dbfb80453806575e49b80484e7d073ba77b162b
Youngmook Lim <youngmookk@gmail.com>
2022-06-13 01:02:20 +0900

    Set up basic html

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const gameBoard = (function () {
+  const board = ["", "", "", "", "", "", "", "", ""];
+  return { board };
+})();
+
+const Player = (sign) => {
+  this.sign = sign;
+  const getSign = () => {
+    return sign;
+  };
+
+  return { getSign };
+};
+
+const playerX = Player("X");
+console.log(playerX);
